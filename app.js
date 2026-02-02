e11ef92496f2768f00d54ea80c35ffd9e9530a5f
Sajid Shaikh <sajid0100shaikh@gmail.com>
2026-02-03 00:37:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,9 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/')));
 app.use(cors())
 
-mongoose.connect('mongodb+srv://supercluster.d83jj.mongodb.net/superData', {
+mongoose.connect(
+  'mongodb+srv://superuser:SuperPassword@supercluster.d83jj.mongodb.net/superData',
+  {
     user: superuser,
     pass: SuperPassword,
     useNewUrlParser: true,
